f27f55ae0401efc3b89489a60c427e9eabe59df7
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-08-14 13:03:43 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is my main function
+ *
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+		putchar(y + '0');
+	putchar("\n");
+	return (0);
+}
