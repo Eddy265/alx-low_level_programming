8589605d5b4dee0b9698c6181ecac387e6ffe7bc
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-08-14 12:08:48 -0700

    print alphabet with condition

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is my main function
+ *
+ * Return: returns 0 if successfull
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	putchar ('\n');
+	return (0);
+}
