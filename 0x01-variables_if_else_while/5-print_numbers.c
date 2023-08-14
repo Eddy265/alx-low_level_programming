7167d0c372ff3881af3c09f26db55d8ec429c47d
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-08-14 12:55:34 -0700

    single digit numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This is my main function
+ *
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", i);
+	return (0);
+}
