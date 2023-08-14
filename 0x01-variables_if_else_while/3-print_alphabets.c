97875c738c04c743683808564ef16fd58199550d
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-08-14 11:50:40 -0700

    print small and capital letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This is my main function
+ *
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	chai i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
