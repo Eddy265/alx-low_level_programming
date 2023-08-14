86c275f1e018c171700c7752c6586c96659f3eb5
Edwin Uzoefuna <euzoefuna@rocketmail.com>
2023-08-14 13:11:25 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is my main function
+ *
+ * Return: returns 0 is successfull
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a  >= 'a'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
